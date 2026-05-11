979733a5fbe5d5f92c8299752e1feb36e9658e0f
boborapido <rapido@presto,fr>
2026-05-11 11:47:46 +0200

    add widget to display paths

---
+++ app/pathstreeview.py
@@ -0,0 +1,16 @@
+from tkinter import Tk, ttk
+
+
+class PathsTreeView:
+
+	def add(self, parent_frame: ttk.Frame, at_grid_row, at_grid_column, colspan):
+		treeview = ttk.Treeview(parent_frame)
+		treeview.grid(row=at_grid_row, column=at_grid_column, columnspan=colspan, sticky="news")
+		# treeview.insert('', '0', 'item2', text ='Computer Science')
+		# treeview.insert('', '1', 'item3', 
+		# 				text ='GATE papers')
+		# treeview.insert('', 'end', 'item4',
+		# 				text ='Programming Languages')
+		# treeview.move('item3', 'item2', 'end')
+		# treeview.move('item4', 'item2', 'end')
+		return self
--- app/wmain.py
+++ app/wmain.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, ttk
+from app.pathstreeview import PathsTreeView
 from file_selector import FileSelector
 
 
@@ -10,28 +11,24 @@ class MainWidget:
 	__file_selector_left: FileSelector = None
 	__file_selector_right: FileSelector = None
 	__frame = None
+	__path_treeview_left: PathsTreeView = None
+	__path_treeview_Right: PathsTreeView = None
 	__scan_button = None
 	__window = None
 
 	def display(self, width, height):
-		# Main window
-		window = Tk()
-		window.title(self.__WINDOW_TITLE)
-		self.__frame = ttk.Frame(
-			window,
-			height=height,
-			name="main_widget",
-			padding=8,
-			width=width,
-		)
-		self.__frame.grid()
+		self.__buildwindow()
+		self.__buildframe(width=width, height=height)
+		# Define UI rows' behavior
+		self.__frame.rowconfigure(1, weight=1)
 		# File Selectors
 		self.__file_selector_left = FileSelector().add(self.__frame, self.__FILE_SELECTORS_ROW, 0)
 		self.__file_selector_right = FileSelector().add(self.__frame, self.__FILE_SELECTORS_ROW, 3)
 		# Scan Button
 		self.__scan_button = ttk.Button(self.__frame, text="Compare").grid(column=2, row=0)
-		# Done
-		self.__window = window
+		# TreeViews
+		self.__path_treeview_left = PathsTreeView().add(self.__frame, 1, 0, 2)
+		self.__path_treeview_right = PathsTreeView().add(self.__frame, 1, 3, 2)
 
 	def mainloop(self):
 		if self.__window == None:
@@ -39,3 +36,19 @@ class MainWidget:
 				"MainWidget is not displayed, so mainloop() cannot be invoked"
 			)
 		self.__window.mainloop()
+		
+	def __buildframe(self, width, height):
+		self.__frame = ttk.Frame(
+			self.__window,
+			height=height,
+			padding=8,
+			width=width,
+		)
+		self.__frame.grid(row=0, column=0, sticky="news")
+
+	def __buildwindow(self):
+		self.__window = Tk()
+		self.__window.title(self.__WINDOW_TITLE)
+		self.__window.columnconfigure(0, weight=1)
+		self.__window.grid_rowconfigure(0, weight=1)
+	
