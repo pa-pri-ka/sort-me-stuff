470233f1ba651e07f25644e68e9f64bc45573d4f
boborapido <rapido@presto,fr>
2026-05-12 10:58:28 +0200

    fix Entry widget expansion in fileselector

--- app/fileselector.py
+++ app/fileselector.py
@@ -11,6 +11,6 @@ class FileSelector:
 	def Add(self, parentframe: ttk.Frame, atgridrow, atgridcolumn):
 		parentframe.grid_columnconfigure(atgridcolumn, weight=1)
 		self._Path = StringVar()
-		ttk.Entry(parentframe, textvariable=self._Path, width=50).grid(column=atgridcolumn, row=atgridrow)
+		ttk.Entry(parentframe, textvariable=self._Path, width=50).grid(column=atgridcolumn, row=atgridrow, sticky="news")
 		ttk.Button(parentframe, text=self._BUTTON_LABEL).grid(column=atgridcolumn + 1, row=atgridrow)
 		return self
